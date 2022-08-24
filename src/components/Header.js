1e47fb3982d55579f87f4b01336fcb25f7e6a600
Dhiraj Surana <dhirajsurana@Punits-Macbook-Air.local>
2022-08-24 16:33:18 +0530

    initial commit [24.08.2022]

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from "styled-components"
+
+function Header() {
+    return (
+        <Container>
+            {/* <h1>header</h1> */}
+        </Container>
+    )
+}
+
+export default Header;
+
+const Container = styled.div`
+
+`
