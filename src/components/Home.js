5bdb620e812b0e02ceb3d988bd7a334b4d38071b
Dhiraj Surana <dhirajsurana@Punits-Macbook-Air.local>
2022-08-25 16:40:02 +0530

    added sections [25.08.2022]

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,82 @@
 import React from 'react'
 import styled from "styled-components"
 import Section from "./Section"
- 
+
+// const details = [
+//   {
+//     "title": "Model S",
+//     "description": "Order Online for Touchless Delivery",
+//     "image": "model-s.jpg"
+//   },
+//   {
+//     "title": "Model 3",
+//     "description": "Order Online for Touchless Delivery",
+//     "image": "model-3.jpg"
+//   },
+//   {
+//     "title": "Model X",
+//     "description": "Order Online for Touchless Delivery",
+//     "image": "model-x.jpg"
+//   },
+//   {
+//     "title": "Model Y",
+//     "description": "Order Online for Touchless Delivery",
+//     "image": "model-y.jpg"
+//   }
+// ]
+
 function Home() {
   return (
-      <Container>
-          {/* <p>h</p> */}
-          <Section/>
+    <Container>
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        image="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        image="model-3.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        image="model-x.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        image="model-y.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Lowest Cost solar panel in america"
+        description="Money-back guarantee"
+        image="solar-panel.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+      />
+      <Section
+        title="Solar for New Roofs"
+        description="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+        image="solar-roof.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Accessories"
+        description=""
+        image="accessories.jpg"
+        leftBtnText="Custom order"
+        rightBtnText=""
+      />
     </Container>
   )
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,22 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 
-function Section() {
-    return (
-        <Wrap>
-            <ItemText>
-                <h1>Model s</h1>
-                <p>Order Online for Touchless Delivery</p>
-            </ItemText>
-            <ButtonGroup>
-                <LeftButton>Custom Order</LeftButton>
-                <RightButton>Existing Inventory</RightButton>
-            </ButtonGroup>
-            <DownArrow src="/images/down-arrow.svg">
-
-            </DownArrow>
-        </Wrap>
-    );
+function Section({ title, description, image, leftBtnText, rightBtnText }) {
+  return (
+    <Wrap bgImage={image}>
+      <ItemText>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg" alt="">
+        </DownArrow>
+      </Buttons>
+    </Wrap>
+  );
 }
 
 export default Section;
@@ -27,7 +28,7 @@ const Wrap = styled.div`
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  background-image: url("/images/model-s.jpg");
+  background-image: ${props => `url("images/${props.bgImage}")`};
   display:flex;
   align-items:center;
   flex-direction: column;
@@ -47,23 +48,40 @@ const ButtonGroup = styled.div`
 
 const LeftButton = styled.div`
   background-color: rgba(23, 26, 32, 0.8);
+  color: white;
   height: 40px;
   width: 256px;
-  color: white;
   display: flex;
   justify-content: center;
   align-items: center;
   border-radius: 100px;
-  opacity: 0.85;
+  opacity: 0.65;
   text-transform: uppercase;
   font-size: 12px;
   cursor: pointer;
+  &:hover{
+    background-color:white;
+    color:rgba(23, 26, 32, 0.8)
+  }
 `;
 
 const RightButton = styled(LeftButton)`
-margin-top:15px;
-background-color:
+  margin-top:15px;
+  background-color: rgba(23, 26, 32, 0.8);
+  color: white;
+
+   &:hover{
+    background-color:white;
+    color:rgba(23, 26, 32, 0.8);
+  }
 `;
 
-const DownArrow = styled.div`
-` 
+const DownArrow = styled.img`
+  height:40px;  
+  animation:animateDown infinite 1.5s;
+  overflow-x:hidden;
+`
+
+const Buttons = styled.div`
+
+`
