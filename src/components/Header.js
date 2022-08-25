9ef772a0577ce1492c5c7fad9653883036bb2761
Dhiraj Surana <dhirajsurana@Punits-Macbook-Air.local>
2022-08-25 18:22:32 +0530

    added header [25.08.2022]

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,70 @@
-import React from 'react'
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
 function Header() {
     return (
         <Container>
-            {/* <h1>header</h1> */}
+            <a>
+                <img src="/images/logo.svg" alt="" />
+            </a>
+            <Menu>
+
+                <a href="#">Model S</a>
+
+
+                <a href="#">Model 3</a>
+
+
+                <a href="#">Model X</a>
+
+
+                <a href="#">Model Y</a>
+
+            </Menu>
+            <RightMenu>
+
+                <a>Shop</a>
+
+
+                <a>Tesla Account</a>
+
+            </RightMenu>
         </Container>
-    )
+    );
 }
 
 export default Header;
 
 const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+`;
 
+const Menu = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content:center ; 
+    flex:1;
+
+    a{
+        font-weight:500;
+        text-transform:uppercase;
+        padding: 0 10px;
+        flex-wrap: no-wrap;
+    }
+`;
+
+const RightMenu = styled.div`
+    display:flex;
+    align-items:center;
+    justify-content:center;  
+
+    a{
+        text-transform:uppercase;
+        padding: 0 10px;
+        font-weight:400;
+    }
 `
